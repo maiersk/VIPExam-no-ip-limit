6e01f393a4a254b302861ca73f5987abf8073812
maiersk <poilyoungman@foxmail.com>
2022-07-23 17:18:13 +0800

    update: 0.2.0

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         vipexam no ip limit
 // @namespace    https://github.com/maiersk
-// @version      0.1
+// @version      0.2.0
 // @description  中科vipexam no ip limit
 // @author       ABTTEX
 // @match        http://www.vipexam.org/*
@@ -10,102 +10,105 @@
 // @match        http://lib.vipexam.cn/*
 // @match        http://vipexam.org/*
 // @match        http://vipexam.cn/*
-// @require      https://code.jquery.com/jquery-3.4.1.min.js
+// @match        http://www.umajor.org/*
+// @require      https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     var _vipexam = null;
+    var _umajor = null;
 
-    layui.use(['form','layer','jquery','cookie'], function(){
+    var _veuser = { code: "", user: { "account": "f46354b31ec64c9c96919e12a9bbd610", "username": "游客（受限）", collegename: "北京大学" } };
+    var _umuser = { code: "1", user: { "account": "f46354b31ec64c9c96919e12a9bbd610", "username": "游客（受限）", collegename: "1111" } };
+
+    layui.use(['form', 'layer', 'jquery', 'cookie', 'element'], function () {
         var $ = layui.jquery,
+            element = layui.element,
             form = layui.form,
             layer = layui.layer,
-            cookie = layui.cookie;
 
         _vipexam = $.cookie("vipexam");
-        var _storage = !!_vipexam ? JSON.parse(_vipexam) : _veuser;
-        var _vid = _storage.user.account;
+        _umajor = $.cookie("umajor");
+        var vipexam_storage = !!_vipexam ? JSON.parse(_vipexam) : _veuser;
+        var umajor_storage = _umajor ? JSON.parse(_umajor) : _umuser;
+        var _vid = vipexam_storage.user.account;
+        var _umid = umajor_storage.user.account;
 
         function RequestJson(url, params, callback) {
             var _idx = layer.load(1);
-            $.ajax({ type: "post", data: params, url: url, dataType: "json", global: false, success: function (data) { callback && callback(data); }, complete: function () { layer.close(_idx); },error:function (XMLHttpRequest, textStatus, errorThrown){layer.close(_idx);} });
+            $.ajax({ type: "post", data: params, url: url, dataType: "json", global: false, success: function (data) { callback && callback(data); }, complete: function () { layer.close(_idx); }, error: function (XMLHttpRequest, textStatus, errorThrown) { layer.close(_idx); } });
         }
 
-        //代替原有登录function
-        form.on('submit(crashlogin)', function(data){
+        //vipexam登录
+        form.on('submit(vipexam_crashlogin)', function (data) {
             _vid = $("#username").val();
-            //ipValidates();
-            RequestJson("user/login.action",{account:$("#username").val(),password:$("#password").val()},function(data){
-                if(data.code == "1"){
-                    //if(_flag){
-                        //alert(JSON.stringify(data))
-                        $.cookie("vipexam", JSON.stringify(data), {  path: '/', expires: 7});
-                        _vipexam = $.cookie("vipexam");
-                        document.location = "index.html";
-                    //}else{
-                    //    layer.msg("抱歉！IP:"+_ip+" 不在系统授权范围内！",{icon:2});
-                    //}
-                }else{
-                    layer.msg(data.msg,{icon:2});
+
+            RequestJson("user/login.action", { account: $("#username").val(), password: $("#password").val() }, function (data) {
+                if (data.code == "1") {
+                    $.cookie("vipexam", JSON.stringify(data), { path: '/', expires: 7 });
+                    _vipexam = $.cookie("vipexam");
+                    document.location = "index.html";
+                } else {
+                    layer.msg(data.msg, { icon: 2 });
+                }
+            });
+            return false;
+        });
+
+        // umajor登录
+        form.on('submit(umajor_crashlogin)', function (data) {
+            _umid = $("#username").val();
+
+            RequestJson("user/login.action", { account: $("#username").val(), password: $("#password").val() }, function (data) {
+                if (data.code == "1") {
+                    $.cookie("umajor", JSON.stringify(data), { path: '/', expires: 7 });
+                    _umajor = $.cookie("umajor");
+                    document.location = "index.html";
+                } else {
+                    layer.msg(data.msg, { icon: 2 });
                 }
             });
             return false;
         });
 
         //关闭ip问题弹窗
-        $(function(){
+        $(function () {
             setTimeout(function () {
                 layer.closeAll();
             }, 350);
         });
 
         //显示已登录信息
-        if(_vid != "f46354b31ec64c9c96919e12a9bbd610"){
-            $("#person").text(_vid);
-            $("#rtbox").hide();
-            $("#rtbox2").show();
+        if (_vid != "f46354b31ec64c9c96919e12a9bbd610" || _umid != "f46354b31ec64c9c96919e12a9bbd610") {
+            if (window.location.host.indexOf('vipexam') !== -1) {
+                $("#person").text(_vid);
+                $("#rtbox").hide();
+                $("#rtbox2").show();
+            }
+
+            if (window.location.host.indexOf('umajor') !== -1) {
+                $("#personid").text(_umid);
+                $("#login").hide();
+                $("#person").show();
+                $("#personid").hover(function () {
+                    layer.tips("<a href='personal_center.html' style='color:#fff;'>个人中心</a>", this, { tips: [3, '#666'], time: 3000, area: ['80px', '40px'] });
+                }, function () { });
+            }
         }
     });
 
-    $(document).ready(function() {
+    $(document).ready(function () {
+        if (window.location.host.indexOf('vipexam') !== -1) {
+            var div = $(".layui-form-item-center");
+            var vipexam_crashlogin = '<div id="vipexam_crashlogin" lay-submit lay-filter="vipexam_crashlogin" class="layui-btn" >crashlogin</div>';
+            div.append(vipexam_crashlogin);
+        }
 
-        var div = $(".layui-form-item-center")
-        var buthtml = '<input type="button" id="crashlogin" lay-submit lay-filter="crashlogin" value="crashlogin" />';
-        //var buthtml = '<input type="button" id="crashlogin" value="crashlogin" />';
-        div.append(buthtml);
-
-        // var but = $("#crashlogin")
-
-        // _vipexam = ""
-
-        // but.click(function(){
-        //     alert(JSON.stringify({account:$("#username").val(),password:$("#password").val()}))
-        //     $.ajax({
-        //         type: "post",
-        //         data: {account:$("#username").val(),password:$("#password").val()},
-        //         url: "user/login.action",
-        //         dataType: "json",
-        //         global: false,
-        //         success: function (data) {
-        //             if(data.code == "1"){
-
-        //                 alert(JSON.stringify(data))
-        //                 $.cookie("vipexam", JSON.stringify(data), {  path: '/', expires: 7});
-        //                 _vipexam = $.cookie("vipexam");
-        //                 document.location = "index.html";
-
-
-        //             }else{
-        //                 layer.msg(data.msg,{icon:2});
-        //             }
-        //         },
-        //         complete: function () {   },
-        //         error:function (XMLHttpRequest, textStatus, errorThrown){ }
-        //     });
-        // })
-
+        if (window.location.host.indexOf('umajor') !== -1 && window.location.pathname.indexOf('login') !== -1) {
+            var umajor_crashlogin = '<div id="umajor_crashlogin" lay-submit lay-filter="umajor_crashlogin" class="layui-btn" >crashlogin</div>';
+            $(".layui-form").append(umajor_crashlogin);
+        }
     })
-
 })();
